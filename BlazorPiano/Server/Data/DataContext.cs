183745b7983024b9ebd87945d8fdc242109b71c4
AdamRGent <adrgent09@hotmail.com>
2023-11-15 16:10:23 +0000

    Display changes

--- BlazorPiano/Server/Data/DataContext.cs
+++ BlazorPiano/Server/Data/DataContext.cs
@@ -22,7 +22,7 @@
         {
             Id = 2,
             Title = "Two Lessons",
-            Descrition = "Two half hour lessons",
+                    Description = "Two half hour lessons",
             ImageUrl = "https://images.unsplash.com/photo-1571974599782-87624638275e?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             Price = 30.99m
         },
